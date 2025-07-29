0ff4b838bf61cbdbfac928a4ea97aa02d77bf95c
kaungwaiyan10 <pinkypiggylay10@gmail.com>
2025-07-29 07:21:41 +0000

    Switch from Vue to React plugin in Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [react()],
+});
